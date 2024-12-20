bcd6d75ff64ea813120cb222c2f184dbb5d26588
Rizea Bogdan-Alexandru <bogdan14610@gmail.com>
2024-12-20 14:45:59 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php  require_once("yaml/yaml.php"); ?>
+<html>
+    
+    <head>
+        <meta charset="utf-8">
+        <title>Portfolio Bogdan</title>
+        <link rel="stylesheet" href="style/style.css">
+    </head>
+
+    <body>
+
+
+
+<?php
+    include("pages/accueil.php");
+    include("pages/compétences.php");
+    include("pages/contact.php");
+    include("pages/formation.php");
+    include("pages/réalisations.php");
+
+?>
+
+    </body>
+</html>
